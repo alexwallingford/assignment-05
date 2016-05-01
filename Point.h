9a1de687aa183928ab3a654f30ca501c7a291174
alexwallingford <alexwallingford007@gmail.com>
2016-04-30 22:43:59 -0700

    Added files via upload

---
+++ Point.h
@@ -0,0 +1,25 @@
+/* ----------------------------------------------------------------------------
+* Copyright &copy; 2016 Alex Wallingford <alexwallingford@csu.fullerton.edu>
+* Released under the [MIT License] (http://opensource.org/licenses/MIT)
+* ------------------------------------------------------------------------- */
+
+ #ifndef POINT_H
+ #define POINT_H
+
+ class Point {
+	 private:
+		 double x;
+		 double y;
+		
+			public:
+					//Constructor: Initializing the point to default (0,0)  
+					 Point(double x = 0, double y = 0);
+			
+						//Functions prototypes  
+					 double getX() const;
+			 double getY() const;
+			 double distance(const Point & a) const;
+				
+};
+ #endif
+
